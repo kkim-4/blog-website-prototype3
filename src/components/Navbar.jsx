89d7fbd5af72974a913647d061bfd9124029ddda
kkim-4 <132178654+kkim-4@users.noreply.github.com>
2024-06-23 18:08:15 -0700

    Add files via upload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,61 +1,75 @@
-import React, {useState} from 'react';
-import menu from '../assets/menu.svg';
-import close from '../assets/close.svg';
-import logo from '../assets/logo.svg'; 
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import SearchBar from './SearchBar.jsx';
+import logo from '../assets/logo1.svg';
 
 const Navbar = () => {
-    const [toggle,setToggle]=useState(false);
-    const handleClick = ()=>setToggle(!toggle)
+    const [toggle, setToggle] = useState(false);
 
+    const handleClick = () => {
+        setToggle(!toggle);
+    };
 
-  return (
-    <div className='w-full h-[80px] z-10 bg-white fixed drop-shadow-lg relative'>
-        <div className='flex justify-between items-center w-full h-full md:max-w-[1240px] m-auto'>
+    const items = ['Home', 'About', 'Support', 'Platform', 'Pricing'];
 
-                <div className='flex items-center'>
-                    <img src={logo} alt="logo" className='sm:ml-10 ss:ml-10 md:ml-3 opacity-[55%] w-full h-[25px]' />
+    return (
+        <>
+            <div className='w-full h-[80px] z-50 bg-white fixed top-0 left-0 drop-shadow-lg'>
+                <div className='flex justify-between items-center w-full h-full md:max-w-[1240px] m-auto'>
+                    <div className='flex items-center'>
+                        <Link to="/">
+                            <img src={logo} alt="logo" className='ml-4 md:ml-3 opacity-[55%] w-[100px] h-[25px]' />
+                        </Link>
+                    </div>
+                    <div className='hidden md:flex items-center'>
+                        <ul className='flex'>
+                            {items.map((item, index) => (
+                                <li key={index} className='mr-14'>
+                                    {item === 'Home' ? (
+                                        <Link to="/" className="text-black">Home</Link>
+                                    ) : item === 'About' ? (
+                                        <Link to="/about-us" className="text-black">About</Link>
+                                    ) : item === 'Support' ? (
+                                        <Link to="/support" className="text-black">Support</Link>
+                                    ) : item === 'Pricing' ? (
+                                        <Link to="/pricing" className="text-black">Pricing</Link>
+                                    ) : item === 'Platform' ? (
+                                        <Link to="/platform" className="text-black">Platform</Link>
+                                    ) : (
+                                        item
+                                    )}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className='hidden md:flex items-center'>
+                        <SearchBar />
+                    </div>
+                    <div className='hidden md:flex items-center'>
+                        <Link to="/login-signup" className='border-none bg-transparent text-black mr-4'>Login</Link>
+                        <Link to="/login-signup" className='px-4 py-2 bg-blue-500 text-white rounded-lg'>Sign Up</Link>
+                    </div>
+                    <div className='md:hidden'>
+                        <button className='bg-transparent text-black mr-4' onClick={handleClick}>Menu</button>
+                    </div>
                 </div>
-          
+                <ul className={toggle ? 'absolute bg-white w-full px-8 md:hidden' : 'hidden'}>
+                    {items.map((item, index) => (
+                        <li key={index} className='py-2'>{item}</li>
+                    ))}
+                    <div className='flex flex-col my-4'>
+                        <Link to="/login-signup" className='bg-transparent text-black mb-4 py-3 px-8'>Login</Link>
+                        <Link to="/login-signup" className='px-8 py-3 bg-blue-500 text-white rounded-lg'>Sign Up</Link>
+                    </div>
+                </ul>
+            </div>
+            <div className='content mt-10 md:mt-20'>
+                {/* Your page content here */}
+            </div>
+        </>
+    );
+};
 
-                <div className='flex items-center'>
-                    <ul className='hidden md:flex'>
-                        <li>Home</li>
-                        <li>About</li>
-                        <li>Support</li>
-                        <li>Platform</li>
-                        <li>Pricing</li>
-                    </ul>
-                </div>
+export default Navbar;
 
 
-
-                <div className='hidden md:flex sm:mr-10 md:mr-10'>
-                    <button className='border-none bg-transparent text-black mr-4'>Login</button>
-                    <button className='px-8 py-3'>Sign Up</button>
-
-                </div>
-
-                <div className='md:hidden' onClick={handleClick}>
-                    <img src={!toggle?menu:close} alt="menu" className='w-[28px] h-[28px] object-contain mr-10' />
-                </div>
-
-                
-
-        </div>
-        <ul className={toggle?'absolute bg-white w-full px-8 md:hidden':'hidden'}>
-                        <li>Home</li>
-                        <li>About</li>
-                        <li>Support</li>
-                        <li>Platform</li>
-                        <li>Pricing</li>
-                        <div className='flex flex-col my-4'>
-                            <button className='bg-transparent text-black mb-4 py-3 px-8'>Login</button>
-                            <button className='px-8 py-3'>Sign Up</button>
-                        </div>
-        </ul>
-
-    </div>
-  )
-}
-
-export default Navbar
